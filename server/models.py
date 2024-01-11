59404387d74b69cbcd8174cc05798dddf0fefcd5
mark9559 <mwangimark57@gmail.com>
2024-01-11 15:58:42 +0300

    Completed Assignment

--- server/models.py
+++ server/models.py
@@ -7,3 +7,14 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    image = db.Column(db.String, nullable=False)
+    price = db.Column(db.Numeric(10, 2), nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'image': self.image,
+            'price': float(self.price)  # Convert Decimal to float for JSON serialization
+        }
